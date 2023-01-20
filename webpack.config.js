8317d71b22571ee34b5b6e69ecf0e99a5cf8332e
clissossi <cedric.lissossi@planity.com>
2023-01-20 15:18:42 +0100

    (main) bash script added to install dependencies + add some folders + rename the default lambda.js into lambda_src.ts

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const nodeExternals = require('webpack-node-externals')
 module.exports = {
   mode: 'production',
   target: 'node',
-  entry: './lambda.ts',
+  entry: './lambda_src.ts',
   output: {
     path: path.resolve('.'),
     filename: 'lambda.js',
